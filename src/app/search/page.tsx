d51c8e9dc3913b5038687a50dd43b4a943aee3b9
Yoonhwi <ush0105@naver.com>
2024-10-15 17:20:40 +0900

    fixed build error

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -33,8 +33,8 @@ const SearchPage = () => {
   }, [query, currentPage, fetchSearchPosts]);
 
   return (
-    <BaseLayout>
-      <Suspense>
+    <Suspense fallback={<div>Loading ... </div>}>
+      <BaseLayout>
         <div className="flex flex-col gap-4 py-16 items-center">
           <SearchPost />
           <div className="w-full">
@@ -57,8 +57,8 @@ const SearchPage = () => {
             />
           </div>
         </div>
-      </Suspense>
-    </BaseLayout>
+      </BaseLayout>
+    </Suspense>
   );
 };
 
